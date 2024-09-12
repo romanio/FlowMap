7b940af5bc07f84e5ffc1f8df838ecd0bde7e492
romanio <rkositsyn@gmail.com>
2024-09-12 10:42:45 +0500

    Добавьте файлы проекта.

---
+++ FlowMap/FlowMap.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using System.Transactions;
+using System.Windows.Media.Animation;
+
+namespace FlowMapApp
+{
+    public struct XY
+    {
+
+        public double x;
+        public double y;
+    }
+
+    public class FlowMap
+    {
+        public double DIA = 0.025;
+        public double DENG = 1.168;
+        public double DENL = 997.0474354;
+        public double VISL = 0.890082518;
+        public double VISG = 0.01848;
+        public double ANGLE = 90;
+
+        public List<XY> SS = new List<XY>();
+        public List<XY> DB = new List<XY>();
+        public List<XY> SW = new List<XY>();
+        public List<XY> AD = new List<XY>();
+
+
+        double fr(double U, double D, double V)
+        {
+            double RE = U * D / V;
+            double C = 0.046;
+            double X = 0.2;
+
+
+            if (RE <= 1500)
+            {
+                C = 16.0;
+                X = 1;
+            }
+
+            return C * Math.Pow(D * U / V, -X);
+        }
+
+        double FINDULS(double UGS, double ULS0)
+        {
+            double HL = AL / (AL + AG);
+            double UG = UGS / (1 - HL);
+            double FG = fr(UG, DG * DIA, VG);
+            double TAUWG = FG * DENG * UG * UG / 2;
+            double B = DIA * (DENL - DENG) * 9.81 * Math.Sin(ARAD);
+            double LHS = TAUWG * (SG / AG + SI / AL + SI / AG) + B;
+
+            double ULS = ULS0;
+            double EPS = 1e10;
+
+            // Нулевая итерация
+
+            double UL = ULS / HL;
+            double FL = fr(UL, DL * DIA, VL);
+            double TAUWL = FL * DENL * UL * UL / 2;
+            double RHS = TAUWL * SL / AL;
+            double Y1 = LHS - RHS;
+            double DU = 0.001;
+
+            ULS = ULS + DU;
+
+            do
+            {
+
+                UL = ULS / HL;
+                FL = fr(UL, DL * DIA, VL);
+                TAUWL = FL * DENL * UL * UL / 2;
+                RHS = TAUWL * SL / AL;
+                double Y2 = LHS - RHS;
+                double DY = (Y2 - Y1);
+                double ULS1 = ULS;
+
+                ULS = ULS1 - Y2 / (DY / DU);
+                DU = ULS - ULS1;
+                Y1 = Y2;
+                EPS = Math.Abs(RHS - LHS);
+            }
+            while (EPS > 1e-5);
+
+            return ULS;
+        }
+
+        double FINDUGS(double ULS, double UGS0)
+        {
+            double HL = AL / (AL + AG);
+            double UL = ULS / (HL);
+            double FL = fr(UL, DL * DIA, VL);
+            double TAUWL = FL * DENL * UL * UL / 2;
+            double B = DIA * (DENL - DENG) * 9.81 * Math.Sin(ARAD);
+            double LHS = TAUWL * SL / AL - B;
+
+            double UGS = UGS0;
+            double EPS = 1e10;
+
+            // Нулевая итерация
+            double UG = UGS / (1 - HL);
+            double FG = fr(UG, DG * DIA, VG);
+            double TAUWG = FG * DENG * UG * UG / 2;
+            double RHS = TAUWG * (SG / AG + SI / AL + SI / AG);
+            double Y1 = LHS - RHS;
+            double DU = 0.001;
+
+            UGS = UGS + DU;
+
+            do
+            {
+
+                UG = UGS / (1 - HL);
+                FG = fr(UG, DG * DIA, VG);
+                TAUWG = FG * DENG * UG * UG / 2;
+                RHS = TAUWG * (SG / AG + SI / AL + SI / AG);
+                double Y2 = LHS - RHS;
+                double DY = (Y2 - Y1);
+                double UGS1 = UGS;
+
+                UGS = UGS1 - Y2 / (DY / DU);
+                DU = UGS - UGS1;
+                Y1 = Y2;
+                EPS = Math.Abs(RHS - LHS);
+            }
+            while (EPS > 1e-5);
+
+            return UGS;
+        }
+
+        double HD;
+        double HLD;
+        double SI;
+        double AL;
+        double A;
+        double AG;
+        double SG;
+        double SL;
+        double UL;
+        double UG;
+        double DL;
+        double DG;
+        double VL;
+        double VG;
+        double ARAD;
+
+        public void Generate()
+        {
+
+            SS.Clear();
+            SW.Clear();
+            AD.Clear();
+            DB.Clear();
+
+            HD = 0;
+            ARAD = Math.Abs((90 - ANGLE) * Math.PI / 180);
+            VL = VISL * 0.001 / DENL;
+            VG = VISG * 0.001 / DENG;
+
+            do
+            {
+                System.Diagnostics.Debug.WriteLine("HD = " + HD);
+                HD = HD + 0.025;
+                HLD = 2 * HD - 1;
+                SI = Math.Sqrt(1 - HLD * HLD);
+                AL = 0.25 * (Math.PI - Math.Acos(HLD) + HLD * SI);
+                A = Math.PI / 4;
+                AG = A - AL;
+                SG = Math.Acos(HLD);
+                SL = Math.PI - SG;
+                UL = A / AL;
+                UG = A / AG;
+                DL = 4 * AL / SL;
+                DG = 4 * AG / (SG + SI);
+
+                // Calculate SS line
+
+                double C2 = 1 - HD;
+                double UGS = Math.Sqrt(C2 * C2 * AG * (DENL - DENG) * DIA * 9.81 * Math.Cos(ARAD) / (UG * UG * SI * DENG));
+                double ULS = FINDULS(UGS, 1);
+
+                if (UGS >= 0.03)
+                {
+                    SS.Add(new XY { x = UGS, y = ULS });
+                }
+
+                // DB Line
+
+                double CL = 0.046;
+                double N = 0.2;
+
+                ULS = Math.Pow(4 * AG * DIA * 9.81 * Math.Cos(ARAD) * (1 - DENG / DENL) / (SI * CL * UL * UL * Math.Pow(DL * DIA * UL / VL, -N)), 1 / (2 - N));
+                UGS = FINDUGS(ULS, 1);
+                if (HD > 0.5)
+                {
+                    DB.Add(new XY { x = UGS, y = ULS });
+                }
+
+                // SW Line
+                double SSMAX = SS.Max(c => c.y);
+
+                ULS = 1;
+                double EPS = 1e10;
+                int it = 0;
+                do
+                {
+                    UGS = Math.Sqrt(4 * VL * 9.81 * (DENL - DENG) * Math.Cos(ARAD) / (0.01 * DENG * ULS * UL * UG * UG));
+                    double ULS1 = ULS;
+                    ULS = FINDULS(UGS, 1);
+                    EPS = Math.Abs(ULS - ULS1);
+                    it++;
+                }
+                while (EPS > 1e-5 && it < 100);
+
+                if (ULS >= 0.003)
+                    if (ULS <= SSMAX)
+                {
+                    SW.Add(new XY { x = UGS, y = ULS });
+                }
+
+            }
+            while (HD < 1.0);
+
+            HD = 0.35;
+            HLD = 2 * HD - 1;
+            SI = Math.Sqrt(1 - HLD * HLD);
+            AL = 0.25 * (Math.PI - Math.Acos(HLD) + HLD * SI);
+            A = Math.PI / 4;
+            AG = A - AL;
+            SG = Math.Acos(HLD);
+            SL = Math.PI - SG;
+            UL = A / AL;
+            UG = A / AG;
+            DL = 4 * AL / SL;
+            DG = 4 * AG / (SG + SI);
+
+            double UGSS = Math.Sqrt((1 - HD) * (1 - HD) * AG * (DENL - DENG) * DIA * 9.81 * Math.Cos(ARAD) / (UG * UG * SI * DENG));
+            double ULSS = FINDULS(UGSS, 1);
+
+
+            double ULDB = Math.Pow(4 * AG * DIA * 9.81 * Math.Cos(ARAD) * (1 - DENG / DENL) / (SI * 0.046 * UL * UL * Math.Pow(DL * DIA * UL / VL, -0.2)), 1 / (2 - 0.2));
+            double UGDB = FINDUGS(ULDB, 1);
+
+
+            AD.Add(new XY { x = UGDB, y = ULDB });
+
+            foreach (var item in DB)
+            {
+                if (item.x > UGSS)
+                //    if (item.x < UGDB)
+                    {
+                        double ULS = FINDULS(item.x, 1);
+                        AD.Add(new XY { x = item.x, y = ULS });
+                    }
+            }
+
+             AD.Add(new XY { x = UGSS, y = ULSS });
+
+      
+        }
+    }
+}
---
+++ FlowMap/MainWindow.xaml.cs
@@ -0,0 +1,234 @@
+﻿using OxyPlot;
+using OxyPlot.Annotations;
+using OxyPlot.Series;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FlowMapApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [DllImport("dwmapi.dll")]
+        private static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, ref int attrValue, int attrSize);
+
+        private const int DWMWA_USE_IMMERSIVE_DARK_MODE_BEFORE_20H1 = 19;
+        private const int DWMWA_USE_IMMERSIVE_DARK_MODE = 20;
+        private const int DWMWA_WINDOW_CORNER_PREFERENCE = 33;
+
+        public enum DWM_WINDOW_CORNER_PREFERENCE
+        {
+            DWMWCP_DEFAULT = 0,
+            DWMWCP_DONOTROUND = 1,
+            DWMWCP_ROUND = 2,
+            DWMWCP_ROUNDSMALL = 3
+        }
+
+        private static void UseWindowAttributes(IntPtr handle)
+        {
+            if (IsWindows10OrGreater(17763))
+            {
+                var attribute = DWMWA_USE_IMMERSIVE_DARK_MODE_BEFORE_20H1;
+                if (IsWindows10OrGreater(18985))
+                {
+                    attribute = DWMWA_USE_IMMERSIVE_DARK_MODE;
+                }
+
+                int corner = (int)DWM_WINDOW_CORNER_PREFERENCE.DWMWCP_DONOTROUND;
+                int darkmode = 1;
+
+                DwmSetWindowAttribute(handle, DWMWA_WINDOW_CORNER_PREFERENCE, ref corner, sizeof(int));
+                DwmSetWindowAttribute(handle, (int)attribute, ref darkmode, sizeof(int));
+            }
+        }
+
+        private static bool IsWindows10OrGreater(int build = -1)
+        {
+            return Environment.OSVersion.Version.Major >= 10 && Environment.OSVersion.Version.Build >= build;
+        }
+
+        public PlotModel plotModel
+        {
+            get => _plotmodel;
+        }
+        
+        OxyPlot.PlotModel _plotmodel = new OxyPlot.PlotModel();
+
+
+        public double VISL
+        {
+            get => fm.VISL;
+            set => fm.VISL = value;
+        }
+
+        public double VISG
+        {
+            get => fm.VISG;
+            set => fm.VISG = value;
+        }
+
+        public double DENG
+        {
+            get => fm.DENG;
+            set => fm.DENG = value;
+        }
+
+        public double DENL
+        {
+            get => fm.DENL;
+            set => fm.DENL = value;
+        }
+
+        public double DIA
+        {
+            get => fm.DIA;
+            set => fm.DIA = value;
+        }
+
+        public double ANGLE
+        {
+            get => fm.ANGLE;
+            set => fm.ANGLE = value;
+        }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            IntPtr hWnd = new WindowInteropHelper(GetWindow(this)).EnsureHandle();
+            UseWindowAttributes(hWnd);
+            DataContext = this;
+
+            _plotmodel = new PlotModel
+            {
+                Title = "Horizontal Flow Map",
+                TitleFontSize = 12,
+                DefaultFontSize = 11,
+                
+            };
+
+            _plotmodel.Axes.Add(new OxyPlot.Axes.LogarithmicAxis
+            {
+                Position = OxyPlot.Axes.AxisPosition.Bottom,
+                MajorGridlineStyle = LineStyle.Dash,
+                Minimum = 0.03,
+                Maximum = 100,
+                Title = "UGS, m/s",
+                MajorGridlineThickness = 1
+
+            });
+
+            _plotmodel.Axes.Add(new OxyPlot.Axes.LogarithmicAxis
+            {
+                Position = OxyPlot.Axes.AxisPosition.Left,
+                MajorGridlineStyle = LineStyle.Dash,
+                Title = "ULS, m/s",
+                Minimum = 0.003,
+                Maximum = 10,
+                MajorGridlineThickness = 1,
+
+            });
+
+            _plotmodel.Series.Add(new LineSeries { });
+            _plotmodel.Series.Add(new LineSeries { Color = OxyColors.Black, StrokeThickness = 2 });
+            _plotmodel.Series.Add(new LineSeries { Color = OxyColors.Black, StrokeThickness = 2 });
+            _plotmodel.Series.Add(new LineSeries { Color = OxyColors.Black, StrokeThickness = 2 });
+            _plotmodel.Series.Add(new LineSeries { Color = OxyColors.Black, StrokeThickness = 2 });
+            _plotmodel.Series.Add(new LineSeries { Color = OxyColors.Black, StrokeThickness = 2 });
+            _plotmodel.Series.Add(new LineSeries { Color = OxyColors.Black, StrokeThickness = 2 });
+
+            _plotmodel.Series.Add(new LineSeries {});
+            _plotmodel.Series.Add(new LineSeries {});
+            _plotmodel.Series.Add(new LineSeries{ });
+
+            _plotmodel.Legends.Add(new OxyPlot.Legends.Legend { LegendPosition = OxyPlot.Legends.LegendPosition.LeftTop });
+
+
+            ((LineSeries)_plotmodel.Series[1]).Points.Add(new DataPoint(10.668, 0.003048));
+            ((LineSeries)_plotmodel.Series[1]).Points.Add(new DataPoint(4.2672, 0.03048));
+            ((LineSeries)_plotmodel.Series[1]).Points.Add(new DataPoint(3.2004, 0.06096));
+            ((LineSeries)_plotmodel.Series[1]).Points.Add(new DataPoint(0.762, 0.35052));
+            ((LineSeries)_plotmodel.Series[1]).Points.Add(new DataPoint(0.762, 1.46304));
+            ((LineSeries)_plotmodel.Series[1]).Points.Add(new DataPoint(0.9906, 4.2672));
+
+            ((LineSeries)_plotmodel.Series[2]).Points.Add(new DataPoint(0.03048, 0.1524));
+            ((LineSeries)_plotmodel.Series[2]).Points.Add(new DataPoint(1.50208, 0.1524));
+
+            ((LineSeries)_plotmodel.Series[3]).Points.Add(new DataPoint(21.336, 0.003048));
+            ((LineSeries)_plotmodel.Series[3]).Points.Add(new DataPoint(18.288, 0.03048));
+            ((LineSeries)_plotmodel.Series[3]).Points.Add(new DataPoint(11.5824, 0.09144));
+            ((LineSeries)_plotmodel.Series[3]).Points.Add(new DataPoint(12.192, 0.170688));
+            ((LineSeries)_plotmodel.Series[3]).Points.Add(new DataPoint(15.24, 0.3048));
+            ((LineSeries)_plotmodel.Series[3]).Points.Add(new DataPoint(30.48, 0.762));
+            ((LineSeries)_plotmodel.Series[3]).Points.Add(new DataPoint(70.136, 4.2672));
+
+            ((LineSeries)_plotmodel.Series[4]).Points.Add(new DataPoint(2.286, 0.09144));
+            ((LineSeries)_plotmodel.Series[4]).Points.Add(new DataPoint(11.5824, 0.09144));
+
+            ((LineSeries)_plotmodel.Series[5]).Points.Add(new DataPoint(0.03048, 4.2672));
+            ((LineSeries)_plotmodel.Series[5]).Points.Add(new DataPoint(70.136, 4.2672));
+
+            ((LineSeries)_plotmodel.Series[6]).Points.Add(new DataPoint(70.136, 4.2672));
+            ((LineSeries)_plotmodel.Series[6]).Points.Add(new DataPoint(81.9912, 9.144));
+
+ 
+
+            _plotmodel.Annotations.Add(new TextAnnotation{ TextPosition = new DataPoint(0.1, 0.02), Text = "STRATIFIED\nFLOW (SS)", StrokeThickness = 0});
+            _plotmodel.Annotations.Add(new TextAnnotation { TextPosition = new DataPoint(8.5, 0.02), Text = "WAVE\nFLOW (SW)", StrokeThickness = 0 });
+            _plotmodel.Annotations.Add(new TextAnnotation { TextPosition = new DataPoint(0.1, 0.6), Text = "ELONGATED\nBUBBLE\nFLOW (I)", StrokeThickness = 0 });
+            _plotmodel.Annotations.Add(new TextAnnotation { TextPosition = new DataPoint(5, 0.75), Text = "SLUG\nFLOW (I)", StrokeThickness = 0 });
+            _plotmodel.Annotations.Add(new TextAnnotation { TextPosition = new DataPoint(0.1, 6), Text = "DISPERSED FLOW (DB)", StrokeThickness = 0 });
+            _plotmodel.Annotations.Add(new TextAnnotation { TextPosition = new DataPoint(50,0.1), Text = "ANNULAR-\nANNULAR\nMIST FLOW (AD)", StrokeThickness = 0 });
+            _plotmodel.InvalidatePlot(true);
+
+        }
+
+        FlowMap fm = new FlowMap();
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ((LineSeries)_plotmodel.Series[0]).Points.Clear();
+            ((LineSeries)_plotmodel.Series[7]).Points.Clear();
+            ((LineSeries)_plotmodel.Series[8]).Points.Clear();
+            ((LineSeries)_plotmodel.Series[9]).Points.Clear();
+
+
+            fm.Generate();
+
+            for (int iw = 0; iw < fm.SS.Count; ++iw)
+            {
+                ((LineSeries)_plotmodel.Series[0]).Points.Add(new DataPoint(fm.SS[iw].x, fm.SS[iw].y));
+      
+            }
+
+            for (int iw = 0; iw < fm.DB.Count; ++iw)
+            {
+                ((LineSeries)_plotmodel.Series[7]).Points.Add(new DataPoint(fm.DB[iw].x, fm.DB[iw].y));
+            }
+
+                for (int iw = 0; iw < fm.SW.Count; ++iw)
+            {
+                ((LineSeries)_plotmodel.Series[8]).Points.Add(new DataPoint(fm.SW[iw].x, fm.SW[iw].y));
+            }
+
+            for (int iw = 0; iw < fm.AD.Count; ++iw)
+            {
+                ((LineSeries)_plotmodel.Series[9]).Points.Add(new DataPoint(fm.AD[iw].x, fm.AD[iw].y));
+            }
+
+            _plotmodel.InvalidatePlot(true);
+        }
+    }
+}
